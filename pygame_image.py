9097fc1ac588232b77aacaa8268b991b83b5248d
lily <c0a25260de@edu.teu.ac.jp>
2026-06-09 17:22:37 +0900

    演習問題2

--- pygame_image.py
+++ pygame_image.py
@@ -23,16 +23,17 @@ def main():
         
         key_lst=pg.key.get_pressed()
         kk_rct.move_ip(-1,0)
+        x=0
+        y=0
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0,-1)
+            y=-1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0,+1)
+            y=+1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(+1,0)
+            x=-1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+2,0)
-        
-        
+            x=+2
+        kk_rct.move_ip(x,y)
         
         x=tmr%3200
 
